3ce491c4fbb8ba9fb7ded073c93b416ca52d1a68
copycat1024 <tranminhhien102408@gmail.com>
2019-01-20 01:32:17 +0200

    Fix docs build

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   ],
   output: {
     filename: 'bundle.[hash].js',
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, 'docs')
   },
   module: {
     rules: [
